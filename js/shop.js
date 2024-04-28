e78370c1ead2ddd26181dd02761a1c65cf0bfb48
Guifré Puig Ribas <gpuig@brothapps.com>
2024-04-28 09:30:44 +0200

    change var declaration for let or const

--- js/shop.js
+++ js/shop.js
@@ -1,78 +1,78 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
+const products = [
     {
         id: 1,
-        name: 'cooking oil',
+        name: "cooking oil",
         price: 10.5,
-        type: 'grocery',
+        type: "grocery",
         offer: {
             number: 3,
-            percent: 20
-        }
+            percent: 20,
+        },
     },
     {
         id: 2,
-        name: 'Pasta',
+        name: "Pasta",
         price: 6.25,
-        type: 'grocery'
+        type: "grocery",
     },
     {
         id: 3,
-        name: 'Instant cupcake mixture',
+        name: "Instant cupcake mixture",
         price: 5,
-        type: 'grocery',
+        type: "grocery",
         offer: {
             number: 10,
-            percent: 30
-        }
+            percent: 30,
+        },
     },
     {
         id: 4,
-        name: 'All-in-one',
+        name: "All-in-one",
         price: 260,
-        type: 'beauty'
+        type: "beauty",
     },
     {
         id: 5,
-        name: 'Zero Make-up Kit',
+        name: "Zero Make-up Kit",
         price: 20.5,
-        type: 'beauty'
+        type: "beauty",
     },
     {
         id: 6,
-        name: 'Lip Tints',
+        name: "Lip Tints",
         price: 12.75,
-        type: 'beauty'
+        type: "beauty",
     },
     {
         id: 7,
-        name: 'Lawn Dress',
+        name: "Lawn Dress",
         price: 15,
-        type: 'clothes'
+        type: "clothes",
     },
     {
         id: 8,
-        name: 'Lawn-Chiffon Combo',
+        name: "Lawn-Chiffon Combo",
         price: 19.99,
-        type: 'clothes'
+        type: "clothes",
     },
     {
         id: 9,
-        name: 'Toddler Frock',
+        name: "Toddler Frock",
         price: 9.99,
-        type: 'clothes'
-    }
-]
+        type: "clothes",
+    },
+];
 
-// => Reminder, it's extremely important that you debug your code. 
+// => Reminder, it's extremely important that you debug your code.
 // ** It will save you a lot of time and frustration!
-// ** You'll understand the code better than with console.log(), and you'll also find errors faster. 
+// ** You'll understand the code better than with console.log(), and you'll also find errors faster.
 // ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-var cart = [];
+const cart = [];
 
-var total = 0;
+let total = 0;
 
 // Exercise 1
 function buy(id) {
@@ -81,9 +81,7 @@ function buy(id) {
 }
 
 // Exercise 2
-function cleanCart() {
-
-}
+function cleanCart() {}
 
 // Exercise 3
 function calculateTotal() {
@@ -100,13 +98,10 @@ function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
 }
 
-
 // ** Nivell II **
 
 // Exercise 7
-function removeFromCart(id) {
-
-}
+function removeFromCart(id) {}
 
 function open_modal() {
     printCart();
